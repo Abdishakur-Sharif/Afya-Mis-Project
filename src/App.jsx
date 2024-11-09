002a259ffd41c9d0b52ebe0e4a7533632ca9d22b
Abdishakur-Sharif <shakuritoshariff@gmail.com>
2024-11-09 14:43:45 +0300

    almost done with doctor and lab ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ForgotPassword from './Componets/ForgotPassword';
 import Login from './Componets/LogIn';
 import Registration from './Componets/Registration';
+import Doctordashboard from './Componets/Doctordashboard';
+import LabReportForm from './Componets/LabReportForm';
 
 function App() {
 
   return (
     <>
         <Routes>
+          <Route path='/doctordashboard' element={<Doctordashboard />} />
+          <Route path='/labreportform' element={<LabReportForm />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
