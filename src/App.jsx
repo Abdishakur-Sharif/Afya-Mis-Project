3e21cf240f04377c304d9e45209a0db9a81cc91c
Duncan <njorogedun870@gmail.com>
2024-11-11 10:42:51 +0300

    made patient list for doctors and made lab request page for the lab update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ForgotPassword from './Componets/ForgotPassword';
 import Login from './Componets/LogIn';
 import Registration from './Componets/Registration';
+import Patient from './Componets/patients';
+import TestRequestsPage from './Componets/testrequest';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
           <Route path="/registration" element={<Registration />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/patients" element={<Patient/>} />
+          <Route path="/testrequests" element={<TestRequestsPage/>} />
         </Routes>
     </>
   )
